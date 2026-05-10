12d27f20d55d33a271e1a4007e31f618140c91fb
thehouseofquantumnetworks <thehouseofquantumnetworks@gmail.com>
2026-05-10 12:06:58 +0500

    supabase

--- README.md
+++ README.md
@@ -14,3 +14,18 @@ The React Compiler is not enabled on this template because of its impact on dev 
 ## Expanding the ESLint configuration
 
 If you are developing a production application, we recommend using TypeScript with type-aware lint rules enabled. Check out the [TS template](https://github.com/vitejs/vite/tree/main/packages/create-vite/template-react-ts) for information on how to integrate TypeScript and [`typescript-eslint`](https://typescript-eslint.io) in your project.
+
+
+<!-- ALTER table todos enable Row level security -->
+
+<!-- create policy "Allow public select"
+on todos
+for select
+using (true) -->
+
+<!-- CREATE POLICY "Allow authenticated insert"
+ON todos
+FOR INSERT
+TO authenticated
+WITH CHECK (true); -->
+
--- src/Componnets/Todo.jsx
+++ src/Componnets/Todo.jsx
@@ -1,25 +1,98 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { supabase } from "../config/supabase-config";
+
 export const Todo = () => {
-  const [todo, setTodo] = useState();
-  const AddTodo = async () => {
-    const { data, error } = await supabase
-      .from("todos")
-      .insert([{ title: todo }]);
-      if(error){
-        console.log(error.message)
-        return
+  const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
+  const [todo, setTodo] = useState([]);
 
-      }
-      alert("Todo Add")
+  // select * from todos;
 
-
+  const getAllTodo = async () => {
+   
+    const { data, error } = await supabase.from("todos").select("*");
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(data);
+       setTodo([])
+      setTodo(data);
+    }
   };
+
+  useEffect(() => {
+    getAllTodo()
+  }, []);
+
+  const AddTodos = async () => {
+    const { data, error } = await supabase.from("todos").insert([
+      {
+        title,
+        description,
+      },
+    ]);
+
+    if (error) {
+      console.log(error);
+      return;
+    }
+    console.log(data);
+    // alert("Add New todo");
+    getAllTodo()
+  };
+
+  const deletTodo = async(id)=>{
+    console.log(id)
+    const {data,error} = await supabase.from("todos").delete().eq("id",id)
+    if(error){
+      console.log(error)
+    }
+    else{
+      alert("delete item")
+      console.log("Test")
+      getAllTodo()
+
+    }
+
+  }
+
+  const updateTodo = async ()=>{
+    const { data,error} = await supabase.from("todos").update({title:"SMIT COourse",description:"test smit new"}).eq("id",22)
+    if(error){
+
+    }
+    else{
+      console.log("done")
+      getAllTodo()
+
+    }
+  }
   return (
     <>
-      <input type="text" value={todo} onChange={(e)=>setTodo(e.target.value)} />
+      <input
+        type="text"
+        value={title}
+        onChange={(e) => setTitle(e.target.value)}
+      />
+      <input
+        type="text"
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+      />
+      <button onClick={() => AddTodos()}>Add Todos</button>
       <br />
-      <button onClick={()=>AddTodo()}>Add todo</button>
+      <ol>
+      {todo.map((v, i) => {
+        return (
+          <li>
+            <b>Title : {v.title}</b>
+            <b>&emsp;description : {v.description}</b>
+            <button onClick={()=>updateTodo()}>Delete iTEM</button>
+
+          </li>
+        );
+      })}
+      </ol>
     </>
   );
 };
